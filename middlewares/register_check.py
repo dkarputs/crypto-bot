67744c58d8b818a9b3badc008315e953181e6e9a
Dmitriy <dkarputs@gmail.com>
2024-05-23 04:12:41 +0200

    Add list top 50, update main menu, update database

--- middlewares/register_check.py
+++ middlewares/register_check.py
@@ -19,12 +19,6 @@ class RegisterCheck(BaseMiddleware):
     ) -> Any:
         user = event.from_user
         if get_user_by_id(user.id, con=data['con']) is None:
-            create_user(user.id, con=data['con'])
+            create_user(user.id, user.username, user.first_name, con=data['con'])
             await data['bot'].send_message(user.id, 'You are registered!')
-
-        # Получаем менеджер сессий из ключевых аргументов, переданных в start_polling()
-        # if not await is_user_exists(user_id=event.from_user.id, session_maker=session_maker, redis=redis):
-        #     await create_user(user_id=event.from_user.id,
-        #                       username=event.from_user.username, session_maker=session_maker, locale=user.language_code)
-        #     await data['bot'].send_message(event.from_user.id, 'Ты успешно зарегистрирован(а)!')
         return await handler(event, data)
