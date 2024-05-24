3699789bd695bc2b2d35527e5e6120af952eff3b
Dmitriy <dkarputs@gmail.com>
2024-05-24 18:19:12 +0200

    Change architectures, add func for my profile, optimize

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,12 +1,20 @@
 from aiogram import Router, types
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, StateFilter
 from aiogram.fsm.context import FSMContext
 
 import utils
+from middlewares.register_check import RegisterCheck
+
+router = Router()
+router.message.middleware(RegisterCheck())
+router.callback_query.middleware(RegisterCheck())
 
 
+@router.message(StateFilter(None), CommandStart())
 async def start_cmd(message: types.Message, state: FSMContext):
     await state.clear()
     user_first_name = message.from_user.first_name
     kb = utils.keyboards.get_main_menu_keyboard()
     await message.answer(f"Hi, {user_first_name}! You are in main menu: ", reply_markup=kb)
+
+
